baffb5c96ec29f9a203fe542eef356b04c274db5
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2023-07-26 20:44:08 +0500

    Initial commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="flex items-center justify-between w-full px-6 py-5 text-white bg-indigo-950 max-w-825 rounded-10"
+  >
+    <div class="flex items-center">
+      <img class="mr-4" src="@/assets/images/svg/light-icon.svg" width="23" height="24" alt="" />
+      <p class="text-lg font-bold mr-9">6 Suggestions</p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <the-header />
+</template>
+
+<script setup lang="ts">
+import TheHeader from '@/components/TheHeader.vue'
+</script>
